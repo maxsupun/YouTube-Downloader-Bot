ecc0b211a59010c83e595223215f5394f6cf7513
hackelite01 <78653024+hackelite01@users.noreply.github.com>
2021-02-26 14:43:52 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/hackelite01")],
+        [InlineKeyboardButton(
+            "Report Bugs ", url="https://t.me/mayank_ka_b_for_bot")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
